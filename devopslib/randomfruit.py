3cb7e1165302759ace6ed88f9735ae78bfa99972
Tim Webster <tim.webster@7digital.com>
2022-08-08 08:30:46 +0000

    adding the lint

---
+++ devopslib/randomfruit.py
@@ -0,0 +1,8 @@
+from random import choices
+
+def fruit():
+    fruits = ["apple", "pear", "banana", "kiwi", "strawberry", "cherry", "blueberry", "peach"]
+    return choices(fruits)[0]
+    
+#x = 1
+#x = x
