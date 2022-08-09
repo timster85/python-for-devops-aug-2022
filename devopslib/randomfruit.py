91ce86f8fdd52e20b862b708ff249081f93f0ce7
Tim Webster <tim.webster@7digital.com>
2022-08-09 08:23:27 +0000

    fixing build

--- devopslib/randomfruit.py
+++ devopslib/randomfruit.py
@@ -1,8 +1,19 @@
 from random import choices
 
+
 def fruit():
-    fruits = ["apple", "pear", "banana", "kiwi", "strawberry", "cherry", "blueberry", "peach"]
+    fruits = [
+        "apple",
+        "pear",
+        "banana",
+        "kiwi",
+        "strawberry",
+        "cherry",
+        "blueberry",
+        "peach",
+    ]
     return choices(fruits)[0]
-    
-#x = 1
-#x = x
+
+
+# x = 1
+# x = x
